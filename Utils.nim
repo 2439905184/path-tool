9459c3fbbcc9dbacdc3f2f220759d43df9a2c9e3
2439905184 <suchenglong018@126.com>
2022-09-11 00:03:40 +0800

    🎈 perf: 改进添加命令组
    软链接模式 避免路径重复

--- Utils.nim
+++ Utils.nim
@@ -1,7 +1,23 @@
 import std/strutils
+import std/sequtils
 
 proc myToWindowsPath*(path: string): string = 
   return path.replace("/", "\\")
 
 proc getWorkMode*(): string = 
   return readFile("etc/mode.txt")
+
+# 此函数用于向txt添加新的行，仅当新的行的字符串和旧行字符串不重复时追加，覆盖旧文件
+proc add_new_line_when_old_file_do_not_include_new_value*(p_file:string, p_newline:string) = 
+  let old:seq[string] = toSeq(lines(p_file))
+  var seqNew = old
+  var newValue = p_newline
+  for v in 0..len(seqNew): #此处使用范围做迭代器，是因为在迭代时不能修改自身变量
+    if not(newValue in seqNew):
+      seqNew.add(newValue)
+  echo "新值: ", seqNew
+  # 写入
+  var f:File = open(p_file,fmWrite)
+  for vv in seqNew:
+    f.write(vv & "\n")
+  f.close()
--- cmd_install.nim
+++ cmd_install.nim
@@ -1,10 +1,17 @@
 import os
 import system/io
+import strformat
+import strutils
+import std/sequtils
+import Utils
 
 # 为名称为xxx的exe文件创建软链接，第二个参数是exe文件的完整绝对路径，如D:\test\test.exe
 proc install_symlink*(p_options:seq[string]) = 
   if len(p_options) > 2:
     echo "错误，参数过多！"
+    return
+  elif len(p_options) == 1:
+    echo "错误，参数过少！"
     return
   var symlinkPath = "/etc/alternatives"
   var configPath = "dpkg/alternatives/"
@@ -13,18 +20,26 @@ proc install_symlink*(p_options:seq[string]) =
   var full_name = name & ".exe"
   var srcPath = path
   var destPath = absolutePath(full_name,root=getCurrentDir() & symlinkPath)
-  echo "计算的src绝对路径: " & srcPath
-  echo "计算的dest绝对路径: " & destPath
-  var f:File = open(configPath & name, fmAppend)
-  f.write(path & "\n")
-  f.close()
-  # 第一个参数是源文件，第二个参数是软链接为文件要放置的路径
-  createSymlink(srcPath, destPath) # 函数原型 createSymlink(src,dest) #把src的文件软链接到dest文件
-
+  
+  if not symlinkExists(fmt"etc/alternatives/{fullname}"):
+    echo "计算的src绝对路径: " & srcPath
+    echo "计算的dest绝对路径: " & destPath
+    createSymlink(srcPath, destPath) # 函数原型 createSymlink(src,dest) #把src的文件软链接到dest文件 第一个参数是源文件的绝对路径，第二个参数是软链接文件的绝对路径
+    var f:File = open(configPath & name & ".txt", fmAppend)
+    f.write(path & "\n")
+    f.close()
+    echo fmt"软链接文件已添加: {destPath}" 
+  else:
+    echo fmt"软链接文件已存在，新路径: {srcPath} 已添加到组: {name}"
+    var old_lines:seq[string] = to_seq(lines(fmt"dpkg/alternatives/{name}.txt"))
+    var new_lines:seq[string] = old_lines
+    echo "--初始--"
+    echo "旧行: " , old_lines
+    echo "新行: " , new_lines
+    add_new_line_when_old_file_do_not_include_new_value(fmt"dpkg/alternatives/{name}.txt",srcPath)
 # 为名称为xxx的路径添加到json文件中 并创建名为xxx的环境变量
 proc install_path*(p_options:seq[string]) = 
   if len(p_options) > 2:
     echo "错误，参数过多！"
     return
-  
   #writeFile(fmt"dpkg/path/{name}.json",json)
