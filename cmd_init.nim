22ce99d23abc9301391883ca267191e53eff0054
2439905184 <suchenglong018@126.com>
2022-09-11 00:18:49 +0800

    🎈 perf: 改进初始化

--- cmd_init.nim
+++ cmd_init.nim
@@ -1,7 +1,11 @@
 import system/io
 import os
+import strformat
+import osproc
 
 proc init_command*() = 
   createDir("dpkg/alternatives")
   createDir("etc/alternatives")
   writeFile("etc/mode.txt","alternatives")
+  var alternativePath = absolutePath("etc/alternatives")
+  discard execCmd(fmt"setx PathToolAlternative {alternativePath}")
