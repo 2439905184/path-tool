4f9f8017378c4729e4d52be2ab286dec0e330265
2439905184 <suchenglong018@126.com>
2022-09-11 00:53:12 +0800

    🎈 perf: 改进删除配置文件中的路径
    path模式

--- cmd_remove.nim
+++ cmd_remove.nim
@@ -1,5 +1,6 @@
 import std/strformat
 import system/io
+import std/json
 
 proc remove*(p_options:seq[string],workMode:string) = 
   if len(p_options) > 2:
@@ -10,7 +11,7 @@ proc remove*(p_options:seq[string],workMode:string) =
   
   var configPath = "dpkg/alternatives/"
   var configFile:string = configPath & name
-  
+
   if workMode == "alternatives":
     var all_path:seq[string] = @[]
     for value in lines(configFile):
@@ -27,9 +28,21 @@ proc remove*(p_options:seq[string],workMode:string) =
 
   if workMode == "path":
     var jsonFile = fmt"dpkg/path/{name}.json"
-    var myJsonNode = parseJson(jsonFile)
-    var all_path = myJsonNode["path"]
-    for key,value in all_path:
-      if value.getStr() == path:
-        all_path.delete(key)
-    #writeFile(jsonFile,)#myJsonNode)
+    var myJsonNode = parseFile(jsonFile)
+    var all_path:seq[JsonNode] = myJsonNode["path"].getElems()
+    var new_path:seq[string] = @[]
+    #echo "--初始--"
+    #echo "all_path: ", all_path
+    # 单独创建一个字符串类型的序列 用于操作和序列化
+    for value in all_path:
+      new_path.add(value.getStr())
+    # 处理新路径数组序列
+    var index = 0
+    for vv in 0..len(new_path):
+      if path in new_path:
+        new_path.delete(index)
+      index += 1
+    echo "--修改后--"
+    var myNewJson = %* {"selfVar":fmt"{name}","path":new_path}
+    echo myNewJson
+    writeFile(jsonFile,$myNewJson)
