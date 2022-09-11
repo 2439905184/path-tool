9826be9aea0ed73ec75e180f947bfb685182c7ce
2439905184 <suchenglong018@126.com>
2022-09-11 11:06:21 +0800

    🎈 perf: 优化程序入口代码

--- src/path_tool.nim
+++ src/path_tool.nim
@@ -10,50 +10,58 @@ var currentWorkMode:string = "alternatives"
 
 if fileExists("etc/mode.txt"):
   currentWorkMode = getWorkMode()
+# 前向声明
+proc main(): void
 
-try:
+when declared(commandLineParams):
+  #echo "有参数传入"
   full_params = commandLineParams()
-  command_type = full_params[0]
-except IndexError:
-  help_command()
-finally:
-  discard
-
-args = full_params[1..len(full_params)-1]
-if len(args) != 0:
-  echo args
-case command_type:
-  of "init":
-    init_command()
-  of "mode":
-    if len(full_params) == 1:
-      echo "当前模式: " & currentWorkMode
-    elif len(full_params) > 1:
-      mode_command(args)
-  of "install":
-    if len(args) > 0:
-      if currentWorkMode == "alternatives":
-        install_symlink(args)
-      if currentWorkMode == "path":
-        install_path(args)
-    elif len(args) == 0:
-      echo "错误，参数过少！"
-  of "config":
-    if currentWorkMode == "alternatives":
-      config_symlink(args)
-    if currentWorkMode == "path":
-      config_path(args)
-  of "remove":
-    remove(args,currentWorkMode)
-  of "remove_all":
-    remove_all(args,currentWorkMode)
-  of "list":
-    if len(args) == 1:
-      list_command(args,currentWorkMode)
-    else: echo "参数过多或者过少！请检查输入"
-  of "version":
-    version_command()
-  of "help":
+  if len(full_params) == 0:
     help_command()
-  else:
-    echo "不支持的命令"
+  elif len(full_params) > 0:
+    command_type = full_params[0]
+    args = full_params[1..len(full_params)-1]
+    main()
+    discard
+else:
+  #help_command()
+  echo "无参数传入"
+proc main() = 
+  if len(args) != 0:
+    echo args
+  case command_type:
+    of "init":
+      init_command()
+    of "mode":
+      if len(full_params) == 1:
+        echo "当前模式: " & currentWorkMode
+      elif len(full_params) > 1:
+        mode_command(args)
+    of "install":
+      if len(args) > 0:
+        if currentWorkMode == "alternatives":
+          install_symlink(args)
+        if currentWorkMode == "path":
+          install_path(args)
+      elif len(args) == 0:
+        echo "错误，参数过少！"
+    of "config":
+      if currentWorkMode == "alternatives":
+        config_symlink(args)
+      if currentWorkMode == "path":
+        config_path(args)
+    of "remove":
+      remove(args,currentWorkMode)
+    of "remove_all":
+      remove_all(args,currentWorkMode)
+    of "list":
+      if len(args) == 1:
+        list_command(args,currentWorkMode)
+      else: echo "参数过多或者过少！请检查输入"
+    of "version":
+      version_command()
+    of "help":
+      help_command()
+    else:
+      echo "不支持的命令"
+      
