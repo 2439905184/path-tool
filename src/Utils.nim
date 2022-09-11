da691eb052807277cc05b8128a71bb94de8a7bfc
2439905184 <suchenglong018@126.com>
2022-09-11 10:04:42 +0800

    🎈 perf: 避免添加命令组时路径重复
    path模式

--- src/Utils.nim
+++ src/Utils.nim
@@ -4,6 +4,9 @@ import std/sequtils
 proc myToWindowsPath*(path: string): string = 
   return path.replace("/", "\\")
 
+proc myToLinuxPath*(path: string): string = 
+  return path.replace("\\","/")
+
 proc getWorkMode*(): string = 
   return readFile("etc/mode.txt")
 
--- src/cmd_install.nim
+++ src/cmd_install.nim
@@ -3,7 +3,9 @@ import system/io
 import strformat
 import strutils
 import std/sequtils
+import std/json
 import Utils
+import osproc
 
 # 为名称为xxx的exe文件创建软链接，第二个参数是exe文件的完整绝对路径，如D:\test\test.exe
 proc install_symlink*(p_options:seq[string]) = 
@@ -33,13 +35,48 @@ proc install_symlink*(p_options:seq[string]) =
     echo fmt"软链接文件已存在，新路径: {srcPath} 已添加到组: {name}"
     var old_lines:seq[string] = to_seq(lines(fmt"dpkg/alternatives/{name}.txt"))
     var new_lines:seq[string] = old_lines
-    echo "--初始--"
-    echo "旧行: " , old_lines
-    echo "新行: " , new_lines
+    #echo "--初始--"
+    #echo "旧行: " , old_lines
+    #echo "新行: " , new_lines
     add_new_line_when_old_file_do_not_include_new_value(fmt"dpkg/alternatives/{name}.txt",srcPath)
+
 # 为名称为xxx的路径添加到json文件中 并创建名为xxx的环境变量
 proc install_path*(p_options:seq[string]) = 
   if len(p_options) > 2:
     echo "错误，参数过多！"
     return
-  #writeFile(fmt"dpkg/path/{name}.json",json)
+  var name:string = p_options[0]
+  var windows_path:string = p_options[1]
+  
+
+  var unix_path = myToLinuxPath(windows_path)
+  #var windows_path = myToWindowsPath(unix_path)
+  var jsonFile = fmt"dpkg/path/{name}.json"
+  var old_paths:JsonNode 
+  var path_json:JsonNode = newJString(unix_path)
+
+  if fileExists(jsonFile):
+    var myJsonNode = parseFile(jsonFile)
+    old_paths = myJsonNode["path"]
+    var oldPath:seq[JsonNode] = old_paths.getElems()
+
+    for value in oldPath:
+      if not(path_json in old_paths):
+        
+        old_paths.add(path_json)
+    writeFile(fmt"dpkg/path/{name}.json",$myJsonNode)
+    #discard execCmd(fmt"setx {name} {windows_path}")
+  #var new_paths:seq[string] = @[]
+  #[for value in old_paths:
+    new_paths.add(value.getStr())
+  for vv in 0..len(old_paths):
+    if not(path in old_paths):
+      new_paths.add(path)
+      ]#
+#[
+  var myJson = %* {"selfVar":fmt"{name}","path":[fmt"{unix_path}{name}"]}
+  
+  var myJson = %* {"selfVar":fmt"{name}","path":new_paths}
+  writeFile(fmt"dpkg/path/{name}.json",$myJson)
+  discard execCmd(fmt"setx {name} {windows_path}")
+  echo fmt"环境变量: {name} 已添加，请手动在path加入%{name}%" ]#
