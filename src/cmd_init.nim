97f890c75eae440e4fdd43f71a1956d02bcfa775
2439905184 <suchenglong018@126.com>
2022-09-13 20:34:41 +0800

    🦄 refactor: 重构了一下初始化代码

--- src/cmd_init.nim
+++ src/cmd_init.nim
@@ -1,17 +1,22 @@
-import system/io
+#import system/io
 import os
 import strformat
 import osproc
 
+proc get_path(relative:string):string = 
+  return absolutePath(relative,root=getAppDir())
+
 proc init_command*() = 
-  var selfPath = getAppDir()
-  createDir(absolutePath("dpkg/alternatives",root=selfPath))
-  createDir(absolutePath("dpkg/path",root=selfPath))
-  createDir(absolutePath("etc/alternatives",root=selfPath))
-  writeFile(absolutePath("etc/mode.txt",root=selfPath),"alternatives")
-  var alternativePath = absolutePath("etc/alternatives",root=selfPath)
+  let selfPath = getAppDir()
+  createDir(get_path("dpkg/alternatives"))
+  createDir(get_path("dpkg/path"))
+  createDir(get_path("etc/alternatives"))
+  writeFile(get_path("etc/mode.txt"),"alternatives")
+
+  var alternativePath = get_path(absolutePath("etc/alternatives"))
   discard execCmd(fmt"setx PathToolAlternative {alternativePath}")
   discard execCmd(fmt"setx PathTool {selfPath}")
   echo "初始化完成"
   echo fmt"PathToolAlternative路径: {alternativePath}"
   echo fmt"PathTool路径: {selfPath}"
+
